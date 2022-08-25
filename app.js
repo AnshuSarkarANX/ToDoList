950285a7d50a698158e0d9c06b7bd26b1026ac39
Anshu Sarkar <anshusarkaranx@gmail.com>
2022-08-25 23:47:37 +0530

    code for server added

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const body = require("body-parser");
+const { application } = require("express");
+const app = express();
+const port = 2;
+app.get("/", (req, res) => {
+  res.write("<h1>Whassupp!</h1>");
+  res.send();
+});
+app.listen(port, () => {
+  console.log("Server running on port ${port}");
+});
