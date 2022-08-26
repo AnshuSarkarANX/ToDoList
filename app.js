4333158228220a7372b1f6524c4558874d87cc2e
Anshu Sarkar <anshusarkaranx@gmail.com>
2022-08-26 23:36:43 +0530

    weekend logic added

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const { application } = require("express");
 const app = express();
 const port = 2;
 app.get("/", (req, res) => {
-  res.write("<h1>Whassupp!</h1>");
-  res.send();
+  var today = new Date();
+  var currentDate = today.getDay();
+  if (currentDate === 6 || currentDate === 0) {
+    res.write("<h1>It's weekend lets   party!</h1>");
+  } else {
+    res.write("<h1>Oh,Work Work Work!!</h1>");
+  }
 });
 app.listen(port, () => {
   console.log("Server running on port " + port);
