a445aecf5dba798f5e8d8ac7aca8e150f22e1a1b
Anshu Sarkar <anshusarkaranx@gmail.com>
2022-08-29 23:34:48 +0530

    app.post added[C

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const body = require("body-parser");
 const { application } = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 const port = 2;
 app.get("/", (req, res) => {
@@ -9,6 +11,11 @@ app.get("/", (req, res) => {
   const options = { weekday: "long" };
   var day = today.toLocaleDateString(undefined, options);
   res.render("list", { KindofDay: day });
+});
+app.post("/", (req, res) => {
+  var work = req.body.work;
+  res.write("<h1>" + work + "</h1>");
+  console.log(work);
 });
 app.listen(port, () => {
   console.log("Server running on port " + port);
