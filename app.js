96b828479206efbdbcd1a1b0037cf1bdc3188590
Anshu Sarkar <anshusarkaranx@gmail.com>
2022-08-30 01:39:02 +0530

    app.post updated, items array added,app.get updated

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const { application } = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+var items = [];
 app.set("view engine", "ejs");
+
 const port = 2;
 app.get("/", (req, res) => {
   var today = new Date();
   const options = { weekday: "long" };
   var day = today.toLocaleDateString(undefined, options);
-  res.render("list", { KindofDay: day });
+  res.render("list", { KindofDay: day, listItem: items });
 });
 app.post("/", (req, res) => {
   var work = req.body.work;
-  res.write("<h1>" + work + "</h1>");
-  console.log(work);
+  items.push(work);
+  res.redirect("/");
 });
 app.listen(port, () => {
   console.log("Server running on port " + port);
