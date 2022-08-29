e7f8a14960e9a3f23cb648aae895c01e1cf9a6c8
Anshu Sarkar <anshusarkaranx@gmail.com>
2022-08-29 11:58:48 +0530

    Switch deleted

--- app.js
+++ app.js
@@ -8,30 +8,6 @@ app.get("/", (req, res) => {
   var today = new Date();
   var currentDate = today.getDay();
   var day = "";
-  switch (currentDate) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error current day is" + currentDate);
-  }
   res.render("list", { KindofDay: day });
 });
 app.listen(port, () => {
