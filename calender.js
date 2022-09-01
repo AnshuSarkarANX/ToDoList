c60e324124b9e593a7b2d98d1075f3a63168b688
Anshu Sarkar <anshusarkaranx@gmail.com>
2022-09-01 21:13:20 +0530

    moved to Local folder

--- calender.js
+++
@@ -1,5 +0,0 @@
-exports.Date = function () {
-  var today = new Date();
-  const options = { weekday: "long", day: "numeric", month: "long" };
-  return today.toLocaleDateString(undefined, options);
-};
