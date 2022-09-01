7458c7cd1f1976c57c3ea2a2b240a6bdca051518
Anshu Sarkar <anshusarkaranx@gmail.com>
2022-09-01 21:02:50 +0530

    renamed

--- date.js
+++
@@ -1,6 +0,0 @@
-module.exports = getDate;
-function getDate() {
-  var today = new Date();
-  const options = { weekday: "long", day: "numeric", month: "long" };
-  return today.toLocaleDateString(undefined, options);
-}
