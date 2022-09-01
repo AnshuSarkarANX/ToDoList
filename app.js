2f7e422aab1d4daeb5f0d68bc49696329b78d184
Anshu Sarkar <anshusarkaranx@gmail.com>
2022-09-01 21:11:55 +0530

    some comments added

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
+//node modules
 const express = require("express");
 const body = require("body-parser");
-const calender = require(__dirname + "/calender.js");
+// local modules
+const calender = require(__dirname + "/Local/calender.js");
 const app = express();
 app.use(body.urlencoded({ extended: true }));
+// arrays
 var items = ["Check emails"];
 var workItems = [];
+// for ejs folder
 app.set("view engine", "ejs");
+// for public folder to access css files
 app.use(express.static("public"));
-
+// server port
 const port = 2;
+// home page
 app.get("/", (req, res) => {
   var day = calender.Date();
   res.render("list", { listTitle: day, listItems: items });
 });
+// for /work page
 app.get("/work", (req, res) => {
   res.render("list", { listTitle: "Work", listItems: workItems });
 });
 app.post("/", (req, res) => {
+  // logic for getting work items
   if (req.body.list === "Work") {
     var workItem = req.body.work;
     workItems.push(workItem);
